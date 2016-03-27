5c88d47d96a409b0b782124d72ab41dfd98095d3
TimMilne <tim.milne@target.com>
2016-03-26 21:42:00 -0500

    Optimized for large runs
    46000 UPCs by 1000 EPCs took an hour.

--- GeniTag/ViewController.m
+++ GeniTag/ViewController.m
@@ -11,11 +11,12 @@
 
 @interface ViewController ()
 {    
-    NSURL      *_inputFileURL;
-    NSURL      *_outputFileURL;
-    int         _startSer;
-    int         _numEach;
-    EPCEncoder *_encode;
+    NSURL          *_inputFileURL;
+    NSURL          *_outputFileURL;
+    NSOutputStream *_outputStream;
+    int             _startSer;
+    int             _numEach;
+    EPCEncoder     *_encode;
 }
 @end
 
@@ -79,16 +80,17 @@
     // Build an output file based on the intput filename and the range
     NSString *fileName = [NSString stringWithFormat:@"GeniTagOutput_%d-%d.csv", _startSer, (_startSer+_numEach-1)];
     _outputFileURL = [[_inputFileURL URLByDeletingLastPathComponent] URLByAppendingPathComponent:fileName];
+    _outputStream = [[NSOutputStream alloc] initWithURL:_outputFileURL append:YES];
+    [_outputStream open];
     
     [_successImg setHidden:TRUE];
     [_failImg setHidden:TRUE];
     
     // Load the input file
-    NSError *error;
+    NSError *error = nil;
     NSString *stringFromInputFile = [NSString stringWithContentsOfURL:_inputFileURL
                                                              encoding:NSUTF8StringEncoding
                                                                 error:&error];
-    NSString *stringForOutputFile = [[NSString alloc] init];
 
     // Double loop to handle both newlines and carriage returns
     for (NSString *file in [stringFromInputFile componentsSeparatedByString:@"\n"]) {
@@ -96,78 +98,85 @@
             // Skip empties
             if ([line length] == 0) continue;
             
-            // Grab the barcode
-            NSString *barcode = line;
-            NSString *hex;
-            
-            // Quick length checks, chop to 12 for now (remove leading zeros)
-            if (barcode.length == 13) barcode = [barcode substringFromIndex:1];
-            if (barcode.length == 14) barcode = [barcode substringFromIndex:2];
-            
-            // Owned brand, encode DPCI in a GID
-            if (barcode.length == 12 && [[barcode substringToIndex:2] isEqualToString:@"49"]) {
-                NSString *dpt = [barcode substringWithRange:NSMakeRange(2,3)];
-                NSString *cls = [barcode substringWithRange:NSMakeRange(5,2)];
-                NSString *itm = [barcode substringWithRange:NSMakeRange(7,4)];
+            // These output arrays can get big, so make sure they are released.
+            @autoreleasepool {
+                NSString *stringForOutputFile = [[NSString alloc] init];
                 
-                // Start with the barcode
-                stringForOutputFile = [stringForOutputFile stringByAppendingString:barcode];
+                // Grab the barcode
+                NSString *barcode = line;
+                NSMutableString *hex;
                 
-                for (int i=0; i<_numEach; i++){
-                    NSString *ser = [NSString stringWithFormat:@"%d", (_startSer+i)];
+                // Quick length checks, chop to 12 for now (remove leading zeros)
+                if (barcode.length == 13) barcode = [barcode substringFromIndex:1];
+                if (barcode.length == 14) barcode = [barcode substringFromIndex:2];
+                
+                // Owned brand, encode DPCI in a GID
+                if (barcode.length == 12 && [[barcode substringToIndex:2] isEqualToString:@"49"]) {
+                    NSString *dpt = [barcode substringWithRange:NSMakeRange(2,3)];
+                    NSString *cls = [barcode substringWithRange:NSMakeRange(5,2)];
+                    NSString *itm = [barcode substringWithRange:NSMakeRange(7,4)];
                     
+                    // Start with the barcode
+                    stringForOutputFile = [stringForOutputFile stringByAppendingString:barcode];
+                    
+                    // Encode the first one
+                    NSString *ser = [NSString stringWithFormat:@"%d", (_startSer)];
                     [_encode withDpt:dpt cls:cls itm:itm ser:ser];
-                    hex = [_encode gid_hex];
+                    hex = [NSMutableString stringWithString:[_encode gid_hex]];
                     
                     // Collect the encoded barcodes
                     stringForOutputFile = [stringForOutputFile stringByAppendingFormat:@",%@", hex];
-                    
                     NSLog( @"\nBarcode: %@\nEncoded GID (hex): %@\n", barcode, hex);
+                    
+                    // Now cycle through the rest
+                    for (int i=1; i<_numEach; i++){
+                        stringForOutputFile = [stringForOutputFile stringByAppendingFormat:@",%@", [self HexIncrement:hex]];
+                        NSLog( @"\nBarcode: %@\nEncoded GID (hex): %@\n", barcode, hex);
+                    }
                 }
-            }
-            
-            // National brand, encode GTIN in an SGTIN
-            else if (barcode.length == 12) {
-                // Take the gtin and encode a reference
-                NSString *gtin = barcode;
                 
-                // Start with the barcode
-                stringForOutputFile = [stringForOutputFile stringByAppendingString:barcode];
-                
-                for (int i=0; i<=_numEach; i++){
+                // National brand, encode GTIN in an SGTIN
+                else if (barcode.length == 12) {
+                    // Take the gtin and encode a reference
+                    NSString *gtin = barcode;
                     
-                    NSString *ser = [NSString stringWithFormat:@"%d", (_startSer+i)];
+                    // Start with the barcode
+                    stringForOutputFile = [stringForOutputFile stringByAppendingString:barcode];
                     
+                    // Encode the first one
+                    NSString *ser = [NSString stringWithFormat:@"%d", (_startSer)];
                     [_encode withGTIN:gtin ser:ser partBin:@"101"];
-                    hex = [_encode sgtin_hex];
+                    hex = [NSMutableString stringWithString:[_encode sgtin_hex] ];
                     
                     // Collect the encoded barcodes
                     stringForOutputFile = [stringForOutputFile stringByAppendingFormat:@",%@", hex];
-                    
                     NSLog( @"\nBarcode: %@\nEncoded SGTIN (hex): %@\n", barcode, hex);
+                    
+                    // Now cycle through the rest
+                    for (int i=0; i<=_numEach; i++){
+                        stringForOutputFile = [stringForOutputFile stringByAppendingFormat:@",%@", [self HexIncrement:hex]];
+                        NSLog( @"\nBarcode: %@\nEncoded SGTIN (hex): %@\n", barcode, hex);
+                    }
                 }
+                
+                // Unsupported barcode
+                else {
+                    NSLog( @"Unsupported Barcode: %@\n", barcode);
+                    continue;
+                }
+                
+                // Write the output file one line at a time and clear the buffer
+                [self writeToOutputFile:stringForOutputFile];
+                stringForOutputFile = nil;
             }
-            
-            // Unsupported barcode
-            else {
-                NSLog( @"Unsupported Barcode: %@\n", barcode);
-                continue;
-            }
-            
-            // Carriage return for next record (the file is all one line)
-            stringForOutputFile = [stringForOutputFile stringByAppendingString:@"\r"];
         }
     }
+    [_outputStream close];
     
-    // Write the output file
-    if ([stringForOutputFile writeToURL:_outputFileURL
-                             atomically:NO
-                               encoding:NSUTF8StringEncoding
-                                  error:&error] == YES)
-    {
+    if (error == nil) {
         [_statusFld setStringValue:@"Success"];
         [_successImg setHidden:FALSE];
-        NSLog( @"Output file written: %@\n", _outputFileURL);
+        NSLog( @"Output file Completed: %@\n", _outputFileURL);
     }
     else
     {
@@ -175,6 +184,58 @@
         [_failImg setHidden:FALSE];
         NSLog( @"Error writing output file: %@\n", error);
     }
+}
+
+- (NSMutableString *)HexIncrement:(NSMutableString *)hex {
+    // Find the last digit that is not an 'f'
+    NSInteger length = hex.length;
+    NSInteger rollDigit = hex.length;
+    NSString *last;
+    do {
+        rollDigit--;
+        if (rollDigit < 0 ) {
+            hex = [NSMutableString stringWithString:@"1"];
+            for (int i=0; i<length; i++) [hex appendString:@"0"];
+            return hex;
+        }
+        last = [hex substringWithRange:NSMakeRange(rollDigit,1)];
+    } while ([last isEqualToString:@"F"]);
+    
+    //Increment
+    NSMutableString *increment;
+    if      ([last isEqualToString:@"0"]) { increment = [NSMutableString stringWithString:@"1"]; }
+    else if ([last isEqualToString:@"1"]) { increment = [NSMutableString stringWithString:@"2"]; }
+    else if ([last isEqualToString:@"2"]) { increment = [NSMutableString stringWithString:@"3"]; }
+    else if ([last isEqualToString:@"3"]) { increment = [NSMutableString stringWithString:@"4"]; }
+    else if ([last isEqualToString:@"4"]) { increment = [NSMutableString stringWithString:@"5"]; }
+    else if ([last isEqualToString:@"5"]) { increment = [NSMutableString stringWithString:@"6"]; }
+    else if ([last isEqualToString:@"6"]) { increment = [NSMutableString stringWithString:@"7"]; }
+    else if ([last isEqualToString:@"7"]) { increment = [NSMutableString stringWithString:@"8"]; }
+    else if ([last isEqualToString:@"8"]) { increment = [NSMutableString stringWithString:@"9"]; }
+    else if ([last isEqualToString:@"9"]) { increment = [NSMutableString stringWithString:@"A"]; }
+    else if ([last isEqualToString:@"A"]) { increment = [NSMutableString stringWithString:@"B"]; }
+    else if ([last isEqualToString:@"B"]) { increment = [NSMutableString stringWithString:@"C"]; }
+    else if ([last isEqualToString:@"C"]) { increment = [NSMutableString stringWithString:@"D"]; }
+    else if ([last isEqualToString:@"D"]) { increment = [NSMutableString stringWithString:@"E"]; }
+    else if ([last isEqualToString:@"E"]) { increment = [NSMutableString stringWithString:@"F"]; }
+    
+    // Pad the increment
+    for (NSInteger i=rollDigit+1; i<length; i++)
+    {
+        [increment appendString:@"0"];
+    }
+    
+    // Replace the tail of the hex number with the increment
+    [hex replaceCharactersInRange:NSMakeRange(rollDigit, length-rollDigit) withString:increment];
+    
+    return hex;
+}
+
+-(void) writeToOutputFile:(NSString*)content{
+    // Carriage return for next record (the file is all one line)
+    content = [content stringByAppendingString:@"\r\n"];
+    NSData *strData = [content dataUsingEncoding:NSUTF8StringEncoding];
+    [_outputStream write:(uint8_t *)[strData bytes] maxLength:[strData length]];
 }
 
 @end
--- README.md
+++ README.md
@@ -6,4 +6,16 @@ in an SGTIN.
 
 Note: The input file should be a csv containing barcodes to be encoded.  You are limited by memory as to how
 many you can create on a single run.  It has been shown that for 1000 barcodes, you can generate up to 40 EPCs
-per barcode per run.  Each run's output file will have the number of EPCs appended to the output file name. 
+per barcode per run.  Each run's output file will have the number of EPCs appended to the output file name.
+
+Here are the run capacities that worked:
+
+1000 UPCs, 40 EPCs each
+6000 UPCs, 5  EPCs each
+
+TPM 3/26/16 Update
+
+I rewrote this and optimized it to write the EPCs as they are generated, with an autoreleasepool code block
+to stop leaking memory, and it can now run indefinitely with an arbitrarily long list of input UPCs generating
+as many EPC's as you'd like, if you have the time.  46000 UPCs, 1000 EPCs each took about an hour to run.
+
